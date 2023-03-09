e0b1e00b8b4698b23efa6d86dc9214abf1bfcb08
illuz <iilluzen@gmail.com>
2023-03-09 14:11:53 +0800

    回滚 schedule，去掉调试代码

--- data_repository/app/schedule/syncAppData.js
+++ data_repository/app/schedule/syncAppData.js
@@ -4,15 +4,15 @@ module.exports = app => {
   return {
     schedule: {
       immediate: true,
-      interval: '60m', // 1 分钟间隔; 2m 30s
+      interval: '60s', // 1 分钟间隔; 2m 30s
       type: 'worker', // 只有一个worker执行
-      disable: false
+      disable: app.config.dataSyncStatus !== '启用',
     },
     async task(ctx) {
       const startTime = new Date().getTime();
       const { logger } = app;
 
-      await ctx.service.tableSync.syncTable({ useSyncTimeSlotFilter: false });
+      await ctx.service.tableSync.syncTable({ useSyncTimeSlotFilter: true });
 
       const endTime = new Date().getTime();
       logger.info('[syncAppData.js]', { useTime: `${endTime - startTime}/ms` });
